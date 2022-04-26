6c83579294ad8a8a104061ac12ac9b8acf487132
Cauê Moalli <50536704+CaueMoalli@users.noreply.github.com>
2022-04-26 14:41:55 -0300

    Add files via upload
    Added several components and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-/* eslint-disable react/react-in-jsx-scope */
-import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -24,12 +24,17 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
